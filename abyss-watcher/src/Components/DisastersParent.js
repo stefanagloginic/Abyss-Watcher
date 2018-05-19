c8ad106348baa313fa7af2abdbaab146b8c91402
stefanagloginic <stefana.gloginic@gmail.com>
2018-05-19 11:51:54 -0700

    added state names on hover for USA, also changed color

--- abyss-watcher/src/Components/DisastersParent.js
+++ abyss-watcher/src/Components/DisastersParent.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import DisastersMap from './DisastersMap';
 import worldData from 'world-atlas/world/110m.json'
 import usData from '../us-110m.json'
-import { geoMercator, geoAlbers } from 'd3-geo'
+import { geoMercator, geoAlbers, geoPath } from 'd3-geo'
 import { select } from 'd3-selection'
 import { feature } from 'topojson-client'
 import * as d3 from 'd3'
@@ -23,18 +23,27 @@ class DisastersParent extends Component {
     	var width = window.innerWidth;
 		var height = window.innerHeight;
 
+		// append first group to the svg
     	select(node)
 	    	.append('g')
 
+	   	// projection type for the map
 	   	var albersProjection = d3.geoMercator()
 		    .scale( width/(1.5*Math.PI) )
 		    .rotate( [0,0] )
 		    .center( [0, 0] )
 		    .translate( [width/2, height/2] );
 
+		// setting projection
 		var geoPath = d3.geoPath()
     		.projection( albersProjection );
+    	// console.log(feature(usData, usData.objects.layer1).features);
 
+    	// reference to the first group
+    	var state_names_g = select(node)
+			.select("g");
+
+		// setup paths, styling & event listeners for map
     	select(node)
 	    	.select('g')
     		.selectAll( "path" )
@@ -44,28 +53,63 @@ class DisastersParent extends Component {
 	    		])
 		    .enter()
 		    .append( "path" )
+		    .attr("class", "state_paths")
+		    .on("mouseover",function (d){
+				state_names_g.append("svg:text")
+					.text(d.properties.postal)
+					.attr("pointer-events", "none")
+					.style("font-size", "3px")
+					.attr("x", function(){
+						return geoPath.centroid(d)[0];
+					})
+					.attr("y", geoPath.centroid(d)[1])
+					.attr("text-anchor","middle")
+					.attr('fill', 'white')
+					.style('font-size', function(){
+						if(d.properties && d.properties.postal){
+							// console.log(geoPath.area(d)/(38*d.properties.postal.length) + "px");
+							return geoPath.area(d)/( 38 * d.properties.postal.length);
+						}
+					})
+					.attr("class", d.properties.postal);
+
+					d3.select(this)
+						.attr( "fill", "#d67f22" )
+						.style("cursor", "pointer"); 
+			})
+			.on("mouseleave",function (d){
+				state_names_g.select("." + d.properties.postal).remove();
+
+				d3.select(this)
+						.attr( "fill", "#00517a" )
+						.style("cursor", "default"); 
+			})
 		    .attr( "fill", "#00517a" )
 		    .attr( "stroke", "#7db4d1")
 		    .attr("stroke-width", 0.5)
 		    .attr( "d", geoPath );
 
+		// append the second group to the DOM
+		state_names_g.append("g")
+			.attr("class", "us_states_names");
+
+		// zoom capability
+		var zoom = d3.zoom()
+		    .scaleExtent([1, 20])
+		    .on("zoom", this.zoomed);
+
 		select(node)
-			.append("rect")
-		    .attr("width", width)
-		    .attr("height", height)
-		    .style("fill", "none")
-		    .style("pointer-events", "all")
-		    .call(d3.zoom()
-		        .scaleExtent([1 / 2, 4])
-		        .on("zoom", this.zoomed.bind(this)));
+			.call(zoom);
     }
 
-    zoomed() {
+
+    zoomed = () => {
     	var node = this.node;
 
 		select(node)
 	    	.select('g')
-	    	.attr("transform", d3.event.transform);
+		 	.style("stroke-width", 1.5 / d3.event.transform.k + "px")
+		  	.attr("transform", d3.event.transform); 
 	}
 
 	render() {
