516117e80046d256f54908ffb5c2725cfbbe13d8
stefanagloginic <stefana.gloginic@gmail.com>
2018-05-29 16:46:43 -0700

    separated out d3 functions to clean up main component

--- abyss-watcher/src/Components/DisastersParent.js
+++ abyss-watcher/src/Components/DisastersParent.js
@@ -9,9 +9,9 @@ import { select } from 'd3-selection'
 import { feature } from 'topojson-client'
 import * as d3 from 'd3'
 import '../Stylesheets/DisastersParent.css'
-import earthquake_data from '../data/earthquake_dataset'
-import tsunami_data from '../data/tsunami_dataset'
 import d3Tip from 'd3-tip'
+import plotTsunamiPoints from '../D3/Tsunami'
+import plotEarthquakePoints from '../D3/Earthquake'
 
 /*----------------Redux---------------------*/
 import { connect } from 'react-redux';
@@ -113,9 +113,9 @@ class DisastersParent extends Component {
 		state_names_g.append("g")
 			.attr("class", "us_states_names");
 
-		this.plotEarthquakePoints();
+		plotEarthquakePoints(node, geoPath, true, this.CreateYearFilter(2016, 2016));
 
-		this.plotTsunamiPoints();
+		plotTsunamiPoints(node, geoPath, true, this.CreateYearFilter(2014, 2016));
 		// zoom capability
 		var zoom = d3.zoom()
 		    .scaleExtent([1, 20])
@@ -132,154 +132,79 @@ class DisastersParent extends Component {
 		}
 	}
 
-    plotTsunamiPoints = () => {
-    	const node = this.node;
-    	const geoPath = this.geoPath;
+  //   plotEarthquakePoints = () => {
+	 //    // add static earthquake group
+	 //    const node = this.node;
+	 //    const geoPath = this.geoPath;
 
-    	var tsunamis = select(node)
-    		.append("g")
-    		.attr("class", "tsunami_points");
+		// var earthquakes = select(node)
+		// 	.append( "g" )
+		// 	.attr("class", "earthquake_points");
 
-    	var yearFilter = this.CreateYearFilter(2015, 2016);
+		// var yearFilter = this.CreateYearFilter(2016, 2016);
 
-    	var tsunami_features = tsunami_data.tsunami_json.features.filter(function(obj) {
-			return yearFilter('1/1/' + obj.properties.Year);
-		}).sort(function(a, b) {
-			return b.properties.Max_height - a.properties.Max_height;
-		});
+		// var earthquake_features = earthquake_data.earthquake_json.features.filter(function(obj) {
+		// 	return yearFilter(obj.properties.Date);
+		// }).sort(function(a, b) {
+		// 	return b.properties.Magnitude - a.properties.Magnitude;
+		// });
 
-		var radius = d3.scaleSqrt()
-		    .domain([0, 10])
-		    .range([0, 12]);
+		// var radius = d3.scaleSqrt()
+		//     .domain([5, 10])
+		//     .range([0, 15]);
 
-		var tip = d3Tip()
-			.attr('class', 'd3-tip-tsunami d3-tip')
-			.offset([-10, 0])
-			.html(function(d) {
-				var location = d.properties.Location.toLowerCase();
-				var period = d.properties.Period == "" ? "Unknown" : d.properties.Period;
-				var date = d.properties.Month + "/" + d.properties.Day + "/" + d.properties.Year;
-			    return "<div style='text-align:center;'><strong style='font-size:15px;color:#84ecff;'>" 
-			    	+ d.properties.Type + "</strong></div>" + 
-			    	"<div><strong style='font-size:12px;'>Date:</strong> <span style='color:red;font-size:12px'>" 
-			    	+ date + "</span></div>" +  
-			    	"<div><strong style='font-size:12px'>Location:</strong> <span style='color:red;font-size:12px'>" 
-			    	+ location + "</span></div>" +
-			    	"<div><strong style='font-size:12px'>Height:</strong> <span style='color:red;font-size:12px'>" 
-			    	+ d.properties.Max_height + "m" + "</span></div>" + 
-			    	"<div><strong style='font-size:12px'>Wave Period:</strong> <span style='color:red;font-size:12px'>" 
-			    	+ period + "</span></div>";
-			});
+		// // setup tooltip
+		// var tip = d3Tip()
+		// 	.attr('class', 'd3-tip-earthquake d3-tip')
+		// 	.offset([-10, 0])
+		// 	.html(function(d) {
+		// 	    return "<div style='text-align:center;'><strong style='font-size:15px;color:#f93f2c;'>" 
+		// 	    	+ d.properties.Type + "</strong></div>" +
+		// 	    	"<div><strong style='font-size:12px;'>Date:</strong> <span style='color:red;font-size:12px'>" 
+		// 	    	+ d.properties.Date + "</span></div>" +  
+		// 	    	"<div><strong style='font-size:12px'>Time:</strong> <span style='color:red;font-size:12px'>" 
+		// 	    	+ d.properties.Time + "</span></div>" +
+		// 	    	"<div><strong style='font-size:12px'>Magnitude:</strong> <span style='color:red;font-size:12px'>" 
+		// 	    	+ d.properties.Magnitude + "</span></div>" + 
+		// 	    	"<div><strong style='font-size:12px'>Depth:</strong> <span style='color:red;font-size:12px'>" 
+		// 	    	+ d.properties.Depth + "km" + "</span></div>";
+		// 	});
 
-		tsunamis.selectAll("circle")
-			.data( tsunami_features )
-			.enter()
-			.append("circle")
-			.attr("transform", function(d) { 
-				return "translate(" + geoPath.centroid(d) + ")"; 
-			})
-	    	.attr("r", function(d){
-	    		return radius(d.properties.Max_height);
-	    	})
-      		.attr("stroke-width", "0.2px")
-      		.attr( "d", geoPath )
-      		.on("mouseover", function(d) {
-	    		d3.select(this)
-					.attr( "fill", "#ffae23" )
-					.attr("fill-opacity", "1")
-					.attr("stroke", "#f9ba4d")
-					.style("cursor", "pointer");
+		// // add earthquake points
+		// earthquakes.selectAll( "circle" )
+		// 	.data( earthquake_features )
+		// 	.enter()
+		// 	.append( "circle" )
+		// 	.attr("transform", function(d) { 
+		// 		return "translate(" + geoPath.centroid(d) + ")"; 
+		// 	})
+	 //    	.attr("r", function(d){
+	 //    		return radius(d.properties.Magnitude);
+	 //    	})
+	 //    	.on("mouseover", function(d) {
+	 //    		d3.select(this)
+		// 			.attr( "fill", "#ac4bb7" )
+		// 			.attr("fill-opacity", "1")
+		// 			.attr("stroke", "#f2cdf7")
+		// 			.style("cursor", "pointer");
 				
-				tip.show(d, this);
-	    	})
-	    	.on("mouseleave", function(d) {
-	    		d3.select(this)
-	    			.attr( "stroke", "#a0efff" )
-					.attr("fill-opacity", ".5")
-					.attr( "fill", "#84ecff" )
-					.style("cursor", "default");
+		// 		tip.show(d, this);
+	 //    	})
+	 //    	.on("mouseleave", function(d) {
+	 //    		d3.select(this)
+	 //    			.attr( "stroke", "#c10000" )
+		// 			.attr("fill-opacity", ".5")
+		// 			.attr( "fill", "#a30000" )
+		// 			.style("cursor", "default");
 				
-				tip.hide(d, this);
-	    	});
+		// 		tip.hide(d, this);
+	 //    	})
+	 //    	.attr("stroke-width", "0.2px")
+		// 	.attr( "d", geoPath );
 
-    	select(node)
-			.call(tip);
-
-    }
-
-    plotEarthquakePoints = () => {
-	    // add static earthquake group
-	    const node = this.node;
-	    const geoPath = this.geoPath;
-
-		var earthquakes = select(node)
-			.append( "g" )
-			.attr("class", "earthquake_points");
-
-		var yearFilter = this.CreateYearFilter(2016, 2016);
-
-		var earthquake_features = earthquake_data.earthquake_json.features.filter(function(obj) {
-			return yearFilter(obj.properties.Date);
-		}).sort(function(a, b) {
-			return b.properties.Magnitude - a.properties.Magnitude;
-		});
-
-		var radius = d3.scaleSqrt()
-		    .domain([5, 10])
-		    .range([0, 15]);
-
-		// setup tooltip
-		var tip = d3Tip()
-			.attr('class', 'd3-tip-earthquake d3-tip')
-			.offset([-10, 0])
-			.html(function(d) {
-			    return "<div style='text-align:center;'><strong style='font-size:15px;color:#f93f2c;'>" 
-			    	+ d.properties.Type + "</strong></div>" +
-			    	"<div><strong style='font-size:12px;'>Date:</strong> <span style='color:red;font-size:12px'>" 
-			    	+ d.properties.Date + "</span></div>" +  
-			    	"<div><strong style='font-size:12px'>Time:</strong> <span style='color:red;font-size:12px'>" 
-			    	+ d.properties.Time + "</span></div>" +
-			    	"<div><strong style='font-size:12px'>Magnitude:</strong> <span style='color:red;font-size:12px'>" 
-			    	+ d.properties.Magnitude + "</span></div>" + 
-			    	"<div><strong style='font-size:12px'>Depth:</strong> <span style='color:red;font-size:12px'>" 
-			    	+ d.properties.Depth + "km" + "</span></div>";
-			});
-
-		// add earthquake points
-		earthquakes.selectAll( "circle" )
-			.data( earthquake_features )
-			.enter()
-			.append( "circle" )
-			.attr("transform", function(d) { 
-				return "translate(" + geoPath.centroid(d) + ")"; 
-			})
-	    	.attr("r", function(d){
-	    		return radius(d.properties.Magnitude);
-	    	})
-	    	.on("mouseover", function(d) {
-	    		d3.select(this)
-					.attr( "fill", "#ac4bb7" )
-					.attr("fill-opacity", "1")
-					.attr("stroke", "#f2cdf7")
-					.style("cursor", "pointer");
-				
-				tip.show(d, this);
-	    	})
-	    	.on("mouseleave", function(d) {
-	    		d3.select(this)
-	    			.attr( "stroke", "#c10000" )
-					.attr("fill-opacity", ".5")
-					.attr( "fill", "#a30000" )
-					.style("cursor", "default");
-				
-				tip.hide(d, this);
-	    	})
-	    	.attr("stroke-width", "0.2px")
-			.attr( "d", geoPath );
-
-		select(node)
-			.call(tip);
-    }
+		// select(node)
+		// 	.call(tip);
+  //   }
 
     zoomed = () => {
     	var node = this.node;
