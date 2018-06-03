e2cbfb6e9061dca839d028dca9bb6038279f457c
stefanagloginic <stefana.gloginic@gmail.com>
2018-06-02 17:34:29 -0700

    added fix for date in json that may not have month/day

--- abyss-watcher/src/D3/Volcano.js
+++ abyss-watcher/src/D3/Volcano.js
@@ -30,11 +30,24 @@ export default (node, geoPath, visible, filter) => {
 		.attr('class', 'd3-tip-volcano d3-tip')
 		.offset([-10, 0])
 		.html(function(d) {
-			var date = d.properties.Month + "/" + d.properties.Day + "/" + d.properties.Year;
+			var date = "";
+			var year = "";
+			var dateStr = "";
+
+			if(d.properties.Month && d.properties.Day) {
+				date = d.properties.Month + "/" + d.properties.Day + "/" + d.properties.Year;
+				dateStr = "<div><strong style='font-size:12px;'>Date:</strong> <span style='color:#faff72;font-size:12px'>" 
+		    	+ date + "</span></div>"
+			}else {
+				year = d.properties.Year;
+				dateStr = "<div><strong style='font-size:12px;'>Year:</strong> <span style='color:#faff72;font-size:12px'>" 
+		    	+ year + "</span></div>"
+			}
+
+
+
 		    return "<div style='text-align:center;'><strong style='font-size:15px;color:#faff72;'>" 
-		    	+ d.properties.Type + "</strong></div>" +
-		    	"<div><strong style='font-size:12px;'>Date:</strong> <span style='color:#faff72;font-size:12px'>" 
-		    	+ date + "</span></div>" +  
+		    	+ d.properties.Type + "</strong></div>" + dateStr +  
 		    	"<div><strong style='font-size:12px'>Name:</strong> <span style='color:#faff72;font-size:12px'>" 
 		    	+ d.properties.Name + "</span></div>" +
 		    	"<div><strong style='font-size:12px'>Elevation:</strong> <span style='color:#faff72;font-size:12px'>" 
